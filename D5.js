0c79a469f932704ea17df160b9174ca099a32e7b
iCarambaaa <88891077+iCarambaaa@users.noreply.github.com>
2022-03-01 10:28:26 +0100

    Update D5.js

--- D5.js
+++ D5.js
@@ -1,11 +1,3 @@
-/*
-ASSIGNMENT RULES
-- All the answers must be written in JavaScript
-- You can ask for help, reach the Teaching Assistants if needed
-- The solution must be available for the tutors by the end of the day (5PM CET)
-- You can Google / use StackOverflow BUT we suggest you to use just the material provided
-*/
-
 /* EXERCISE 1
 Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
 */
@@ -115,7 +107,3 @@ Write a function "giveMeRandom" which accepts a number n and returns an array co
   //  let rn = (n * Math.floor(Math.random() * 10));
     //returns; 
 //}
-
-/* WHEN YOU ARE FINISHED
-Commit and push the code to your personal GitHub repository and share the link to your commit in Eduflow.
-*/
